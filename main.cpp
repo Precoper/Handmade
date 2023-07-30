14053c7e925896528c05d90f828bd38a996fbdbe
Precoper <130590261+Precoper@users.noreply.github.com>
2023-07-30 15:33:55 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,198 @@
+#include <iostream>
+
+using namespace std;
+
+void say() {
+	cout << "this is a function";
+}
+
+int main() {
+
+	//auto sayfunc = say;
+
+	// the auto type is the type describe below it consist of function_return_type(*function_name) (parameters); this  is a pointer that points to where the function is stored
+	// it is important to note that this is just a declaration and should be assigned to a function (more precisely a function address) in order to work
+
+
+
+	//void(*funcsay) () = say;
+
+
+
+	//typedef void(*saying)();
+
+	//saying  funcsay = say;
+
+	//funcsay();
+
+
+
+	//int choice = 0;
+
+	
+	
+	
+	
+
+	struct order {
+		string order_name = "";
+		int amount;
+		float price;
+
+	}order;
+
+	int order_input, food_input, drink_input;
+	
+
+	enum order_type {
+	    food = 1,
+		drink
+	};
+	
+	enum food_choice {
+		tacobella = 1,
+		burger,
+	};
+
+	enum drink_choice {
+		coffee = 1,
+		tea,
+		coca
+	};
+
+	
+	
+	bool exit_loop = false;
+	
+	while (exit_loop == false) {
+		
+
+		cout << "enter 1 for ordering food and 2 for order drink " << endl << "enter your choice here: " << endl;
+		cin >> order_input;
+		
+		
+		if (order_input == order_type::drink) {
+			int amount;
+
+			while (true) {
+
+
+				cout << "drinks: " << endl
+					<< "press 1 for coffee  " << endl
+					<< "press 2 for tea " << endl
+					<< "press 3 for coca " << endl
+					<< "enter your choice here: "
+					<< endl;
+
+				cin >> drink_input;
+
+				cout << "please enter the number of items you want to order: " << endl;
+
+				cin >> amount;
+
+				if (drink_input == drink_choice::coffee) {
+					float base_pirce = 1.2;
+					order.order_name = "coffee";
+					order.amount = amount;
+					order.price += base_pirce * amount;
+					cout << "you just ordered  " << order.amount << ", " << order.order_name << ", which costed you: " << order.price << endl;
+					exit_loop = true;
+					break;
+					
+				}
+				else if (drink_input == drink_choice::tea) {
+					float base_pirce = 1.0;
+					order.order_name = "tea";
+					order.amount = amount;
+					order.price += base_pirce * amount;
+					cout << "you just ordered  " << order.amount << ", " << order.order_name << ", which costed you: " << order.price << endl;
+					exit_loop = true; 
+					break;
+				}
+				else if (drink_input == drink_choice::coca) {
+					float base_pirce = 1.5;
+					order.order_name = "coca";
+					order.amount = amount;
+					order.price += base_pirce * amount;
+					cout << "you just ordered  " << order.amount << ", " << order.order_name << ", which costed you: " << order.price << endl;
+					exit_loop = true;
+					break;
+					
+				}
+				else
+					cout << "please enter a valid input" << endl;
+
+			}
+			
+
+		}
+		else if (order_input == order_type::food) {
+			int amount;
+
+			while (true) {
+				cout << "foods" << endl
+					<< "press 1 for taco bella " << endl
+					<< "press 2 for burger" << endl
+					<< "enter your choice here: "
+					<< endl;
+
+				cin >> food_input;
+
+				cout << "enter the number of items you want to order" << endl;
+
+				cin >> amount;
+
+				if (food_input == food_choice::tacobella) {
+					float base_pirce = 4;
+					order.order_name += "taco bell";
+					order.amount = amount;
+					order.price = base_pirce * amount;
+					cout << "you just ordered  " << order.amount << ", " << order.order_name << ", which costed you: " << order.price << endl;
+					exit_loop = true;
+					break;
+				}
+				else if (food_input == food_choice::burger) {
+					float base_pirce = 5;
+					order.order_name += "burger";
+					order.amount = amount;
+					order.price = base_pirce * amount;
+					cout << "you just ordered  " << order.amount << ", " << order.order_name << ", which costed you: " << order.price << endl;
+					exit_loop = true;
+					break;
+				}
+				else
+					cout << "please enter a valid input" << endl;
+
+
+			}
+
+		}
+		//break;
+		//else
+			//cout << "please enter a valid input: " << endl;
+	}
+	
+	
+	
+	
+	return 0;
+}
+
+
+
+//enum choice --> ordername --> price
+
+/*
+  while (true) {
+    if(true){
+	   while (true){
+	    if false break
+	   
+	   }
+	      
+	
+	}
+  }
+*/ 
+
+
